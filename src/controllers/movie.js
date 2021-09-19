2e728473d2e948e556a186366edb31614c67a924
Ramu <vishwakarma1217@gmail.com>
2021-09-19 16:58:33 +0530

    added try catch and postman collection

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -11,45 +11,71 @@ const {
     notFoundResponse,
 
 } = require('../helpers/apiResponse');
+const logger = require('../logger');
 
 module.exports = {
     getAllMovieHandler: async function (req, res) {
-        const movies = await getAllMovie();
-        return successResponseWithData(res,'Operation success', movies);
+        try {
+            const movies = await getAllMovie();
+            return successResponseWithData(res, 'Operation success', movies);
+        } catch (error) {
+            logger.error('ERROR | getAllMovieHandler', error.message);
+            throw error;
+        }
     },
     createMovieHandler: async function (req, res) {
-        const body = req.body;
+        try {
+            const body = req.body;
 
-        const movie = await createMovie({ ...body });
-        return successResponseWithData(res, 'Movie Created', movie);
+            const movie = await createMovie({ ...body });
+            return successResponseWithData(res, 'Movie Created', movie);
+        } catch (error) {
+            logger.error('ERROR | createMovieHandler', error.message);
+            throw error;
+        }
     },
     updateMovieHandler: async function (req, res) {
-        const _id = req.params.id;
-        const update = req.body;
-        const options = { new: true };
+        try {
+            const _id = req.params.id;
+            const update = req.body;
+            const options = { new: true };
 
-        const movie = await findMovie({ _id });
-        if (!movie) {
-            return notFoundResponse(res, 'Movie not found');
+            const movie = await findMovie({ _id });
+            if (!movie) {
+                return notFoundResponse(res, 'Movie not found');
+            }
+            const updatedMovie = await findAndUpdateMovie({ _id }, update, options);
+            return successResponseWithData(res, 'Movie updated successfully', updatedMovie);
+        } catch (error) {
+            logger.error('ERROR | updateMovieHandler', error.message);
+            throw error;
         }
-        const updatedMovie = await findAndUpdateMovie({ _id }, update, options);
-        return successResponseWithData(res,'Movie updated successfully', updatedMovie);
     },
     getMovieHandler: async function (req, res) {
-        const _id = req.params.id;
-        const movie = await findMovie({ _id });
-        if (!movie) {
-            return notFoundResponse(res, 'Movie not found');
+        try {
+            const _id = req.params.id;
+            const movie = await findMovie({ _id });
+            if (!movie) {
+                return notFoundResponse(res, 'Movie not found');
+            }
+            return successResponseWithData(res, 'Operation success', movie);
+        } catch (error) {
+            logger.error('ERROR | getMovieHandler', error.message);
+            throw error;
         }
-        return successResponseWithData(res,'Operation success', movie);
     },
     deleteMovieHandler: async function (req, res) {
-        const _id = req.params.id;
-        const movie = await findMovie({ _id });
-        if (!movie) {
-            return notFoundResponse(res, 'Movie not found');
+        try {
+            const _id = req.params.id;
+            const movie = await findMovie({ _id });
+            if (!movie) {
+                return notFoundResponse(res, 'Movie not found');
+            }
+            await deleteMovie({ _id });
+            return successResponse(res, 'Movie deleted');
+        } catch (error) {
+            logger.error('ERROR | deleteMovieHandler', error.message);
+            throw error;
         }
-        await deleteMovie({ _id });
-        return successResponse(res, 'Movie deleted');;
     }
 }
